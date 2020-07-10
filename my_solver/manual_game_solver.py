0d3bd8cfb048db2b9cd6ed14ad9774027efa99f8
Umr Barends <ubarends@gmail.com>
2020-07-10 14:39:50 +0200

    Updated manual_game_solver and pieces now moved and displayed correctly

--- my_solver/manual_game_solver.py
+++ my_solver/manual_game_solver.py
@@ -1,13 +1,12 @@
  ### Basic NxN sliding puzzle solver 
 from puzzle_generator import puzzle as puzzle
 import numpy as np 
-from pynput.keyboard import Key, Listener
+#from pynput.keyboard import Key, Listener
 #from threading import Thread
 import tkinter as tk
 from PIL import ImageTk, Image
 
 puz = puzzle()
-arr = puz.blocks
 # function to be called when 
 # keyboard buttons are pressed 
 def key_press(event): 
@@ -16,43 +15,50 @@ def key_press(event):
     if(key == 's'): puz.transpose('down')
     if(key == 'a'): puz.transpose('left')
     if(key == 'd'): puz.transpose('right')
-    print(key, 'is pressed') 
+    if(key == 'q'): 
+        root.destroy()
+        return # return to after root.mainloop() if successfully exited
+    #print(key, 'is pressed')
+    print(puz.blocks)
     base_path = "number images/"
-    photo_val = 1
+    photo_val = 0
     for i in range(4):
+        y = i*50
         for j in range(4):
-            num_photo = np.where(arr == photo_val)[0][0]
-            path = base_path + str(num_photo+1) + ".png"
+            #num_photo = np.where(puz.blocks == photo_val)[0][0]
+            #print("num_photo",num_photo)
+            photo = puz.blocks[photo_val]
+            path = base_path + str(photo) + ".png"
 
             im = Image.open(path) # PIL image
             tkimage = ImageTk.PhotoImage(im) # tk converted image from PIL image
             panel = tk.Label(root, image=tkimage) # handle and binding of tkimage to root
             panel.image = tkimage # set the panels image to tkimage
 
-            x = (j%4)*50
-            y = (i%4)*50
+            x = j*50 # paste image colum by column, from left to right
             panel.place(x=x,y=y)
             photo_val += 1
 
 root = tk.Tk()
 root.title("Join")
-root.geometry("500x500")
+root.geometry("300x300")
 #root.configure(background='')
 #img  = Image.open("number images/blank_image.png")
 base_path = "number images/"
-photo_val = 1
+photo_val = 0
 for i in range(4):
+    y = i*50
     for j in range(4):
-        num_photo = np.where(arr == photo_val)[0][0]
-        path = base_path + str(num_photo+1) + ".png"
+        #num_photo = np.where(puz.blocks == photo_val)[0][0]
+        photo = puz.blocks[photo_val]
+        path = base_path + str(photo) + ".png"
         
         im = Image.open(path) # PIL image
         tkimage = ImageTk.PhotoImage(im) # tk converted image from PIL image
         panel = tk.Label(root, image=tkimage) # handle and binding of tkimage to root
         panel.image = tkimage # set the panels image to tkimage
 
-        x = (j%4)*50
-        y = (i%4)*50
+        x = j*50
         panel.place(x=x,y=y)
         photo_val += 1
 
