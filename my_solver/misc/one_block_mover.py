d378f635f8572db2f9584324d66ba43d69a2bec9
Umr Barends <ubarends@gmail.com>
2020-07-28 10:58:10 +0200

    Updated a few value function code files

---
+++ my_solver/misc/one_block_mover.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from help_functions import * as hf
+
+sn = np.array([1,2,3,4,5,6,7,8,9])
+
+s = np.array([2,3,1,4,9,5,7,8,6])
+
+blank_index = np.where(s == (size))[0][0]
+
+
---
+++ my_solver/misc/value_function_shortest_path.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# array of states, where each state is a scalar value
+s = np.zeros(16) # a flattened 4x4 matrix representing a singular state
+
+for i in range(len(s)): # loop through all blocks in state
+
+    if (i != 0): # first block is terminal block
+        
+
+
+def check_max_neighbour_blocks(s,block_index):
+    max_val = -1000000
+    ### TODO check if N is odd if the modulus logic still works
+    # First block could also be coded as if block_index == 0""
+    if block_index+1 < N and (block_index+1)%N == 1:
+        # calc max of right and down values
+        pass
+    elif block_index+1 > N and (block_index+1)%N == 1:
+
+        
