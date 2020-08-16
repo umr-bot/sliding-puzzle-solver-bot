43866c380f112016a91b8b6a243e6c0fb4a8782b
Umr Barends <ubarends@gmail.com>
2020-08-16 22:37:06 +0200

    Seem to have a way of encoding the 2x2 sliding puzzle for now. Not sure yet if they is a more efficient method.

--- my_solver/2x2_sliding_puzzle_value_iteration/setup.py
+++ my_solver/2x2_sliding_puzzle_value_iteration/setup.py
@@ -31,7 +31,9 @@ import itertools
     #must still fix:  for state in lt still fix:
     #must still fix:      if is_solvable(state): st still fix.append(state)
     #must still fix:  
-solvable = np.array([[0,1,2,3],[1,3,0,2],[3,2,1,0],[2,0,3,1],[0,1,3,2],[1,2,0,3],[2,3,1,0],[3,0,2,1],[0,3,2,1],[3,1,0,2],[1,2,3,0],[2,0,1,3]])
+solvable = np.array([   [0,1,2,3],[1,3,0,2],[3,2,1,0],[2,0,3,1],
+                        [0,1,3,2],[1,2,0,3],[2,3,1,0],[3,0,2,1],
+                        [0,3,2,1],[3,1,0,2],[1,2,3,0],[2,0,1,3] ])
 
 ### returns a list of numbers formed by concatenating elements of the input
 ### array state
@@ -39,20 +41,57 @@ def encoder(state):
     state_list = state.astype(str).tolist()
     state_list.sort()
     return [''.join(row) for row in state_list]
-
+### repeating what is being done in the encoder function temporarily to be explicit
 m = solvable.astype(str).tolist()
 m.sort()
-n = [''.join(row) for row in m]
+n = [''.join(row) for row in m] # check end of this file to see output of n
 
-look_up = np.zeros(12).reshape(12,1) 
+N = 12 # possible solvable states 4!/2. Total states including unsolvable is 4! = 24
+look_up = np.zeros(N).reshape(N,1)
 for i in range(len(n)): 
     for j in range(len(n[0])): 
         if n[i][j] == '3': look_up[i] = j
-P = np.zeros((12,12))
-for i in range(look_up.shape[0]): # look_up.shape[0] = 4!/2 = 12 for a 2x2 puzzle
-    for j in range(look_up.shape[0]):
-        if np.abs(look_up[i]-look_up[j]) == 1: # pos3(state) - pos3(state + i) i in [0,N-1],N=2
+
+P = np.zeros((N,N))
+
+for i in range(12):
+    for j in range(12):
+        if np.sum(solvable[i] == solvable[j]) >= 2:
             P[i][j] = 1
+
+#for i in range(look_up.shape[0]): # look_up.shape[0] = 4!/2 = 12 for a 2x2 puzzle
+#    for j in range(look_up.shape[0]):
+#        if np.abs(look_up[i]-look_up[j]) <= 1: 
+#            # pos3(state) - pos3(state + i), i element of [0,N-1], here N=2
+#            P[i][j] = 1
+
+#P[1][0][0][0][0] = 1 
+#P[1][0][0][1][0] = 0
+#P[3][0][0][0][0] = 1
+#P[3][0][0][0][1] = 0
+#P[2][0][0][0][0] = 1
+#P[2][0][0][0][1] = 0
+#P[0][0][0][0][0] = 1
+#P[0][0][0][0][1] = 0
+#
+#N = s.shape[0]*s.shape[1]
+#p = P.reshape(len(a),N,N)
+#r = R.reshape(len(a),N)
+#for i in range(len(a)): 
+#    r[i][0] = 0 # set all rewards @ index 0 for all actions to zero
+#v = np.zeros(N) # initial value function value
+#k = 0
+#diff = 10 # arbritary allocation larger than condition 0.001
+#gamma = 1 
+#while diff > 0.001 and k<10: # loop through all blocks in grid
+#    X = r + gamma*p.dot(v)
+#    v_kplus1 =  v
+#    v = np.amax(X,axis=0)
+#    
+#    diff = np.linalg.norm(v-v_kplus1)
+#    print(v.reshape(4,4))
+#    k += 1
+    
 ### Solvable states ###
 #[['0', '1', '2', '3'],
 # ['0', '1', '3', '2'],
