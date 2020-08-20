f3f1ebcdab68b0b32e0c7c00e3a19c3a64197b1c
Umr Barends <ubarends@gmail.com>
2020-08-20 19:04:44 +0200

    Value iteration max operation not functioning as intended yet. Must still correct

--- my_solver/2x2_sliding_puzzle_value_iteration/setup.py
+++ my_solver/2x2_sliding_puzzle_value_iteration/setup.py
@@ -55,6 +55,7 @@ for i in range(len(n)):
 up,down,left,right = [],[],[],[] # sets indicate states which can move in these 
                                  # directions
 states = np.array(m).astype(int)
+a = [0,1,2,3]
 P = np.zeros((4,N,N))
 for i in range(N):
     for j in range(N):
@@ -85,73 +86,27 @@ for i in range(N):
 
                         P[3][i][j] = 1                     # can move right
 
-#P = np.zeros((4,N,N)) # actions x states x states
-#for i in range(12):
-#    for j in range(12):
-#        if np.sum(solvable[i] == solvable[j]) >= 2:
-#            P[i][j] = 1
-
-#################
-#P = np.zeros(len(a)*s.shape[0]*s.shape[1]*s.shape[0]*s.shape[1]) # actions by rows by cols by rows by cols 
-#P = P.reshape(len(a),s.shape[0],s.shape[1],s.shape[0],s.shape[1])
-#for i in range(P.shape[0]):
-#    for j in range(P.shape[1]):
-#        if(i == 0): # first row (can't move up)
-#            P[0][i][j][i][j] = 1
-#        else: P[0][i][j][i-1][j] = 1 # other rows (can move up)
-#        
-#        if(i == P.shape[0]-1): # last row (can't move down)
-#            P[1][i][j][i][j] = 1
-#        else: P[1][i][j][i+1][j] = 1 # other rows (can move down)
-#
-#        if(j == 0): # first column (can't move left)
-#            P[2][i][j][i][j] = 1
-#        else: P[2][i][j][i][j-1] = 1 # other columns (can move left)
-#
-#        if(j == P.shape[1]-1): # last column (can't move right)
-#            P[3][i][j][i][j] = 1
-#        else: P[3][i][j][i][j+1] = 1 #other columns (can move right)
-## make down and right transitions from the zeroth block zero
-## and instead then make transistions from the zeroth block to itself
-## equal to 1 (value 1 represents possible movement)
-#P[1][0][0][0][0] = 1 
-#P[1][0][0][1][0] = 0
-#P[3][0][0][0][0] = 1
-#P[3][0][0][0][1] = 0
+P[0][0][3] = 0 #'blank up' possible but make it zero since in terminal state
+P[2][0][1] = 0 #'blank right' possible but make it zero since in terminal state
 #################
 
-#for i in range(look_up.shape[0]): # look_up.shape[0] = 4!/2 = 12 for a 2x2 puzzle
-#    for j in range(look_up.shape[0]):
-#        if np.abs(look_up[i]-look_up[j]) <= 1: 
-#            # pos3(state) - pos3(state + i), i element of [0,N-1], here N=2
-#            P[i][j] = 1
-
-#P[1][0][0][0][0] = 1 
-#P[1][0][0][1][0] = 0
-#P[3][0][0][0][0] = 1
-#P[3][0][0][0][1] = 0
-#P[2][0][0][0][0] = 1
-#P[2][0][0][0][1] = 0
-#P[0][0][0][0][0] = 1
-#P[0][0][0][0][1] = 0
-#
-#N = s.shape[0]*s.shape[1]
-#p = P.reshape(len(a),N,N)
-#r = R.reshape(len(a),N)
-#for i in range(len(a)): 
-#    r[i][0] = 0 # set all rewards @ index 0 for all actions to zero
-#v = np.zeros(N) # initial value function value
-#k = 0
-#diff = 10 # arbritary allocation larger than condition 0.001
-#gamma = 1 
-#while diff > 0.001 and k<10: # loop through all blocks in grid
-#    X = r + gamma*p.dot(v)
-#    v_kplus1 =  v
-#    v = np.amax(X,axis=0)
-#    
-#    diff = np.linalg.norm(v-v_kplus1)
+R = -1*np.ones(len(a)*N).reshape(len(a),N)
+for i in range(len(a)): 
+    R[i][0] = 0 # set all rewards @ index 0 for all actions to zero
+v = np.zeros(N) # initial value function value
+k = 0
+diff = 10 # arbritary allocation larger than condition 0.001
+gamma = 1
+arr = []
+while diff > 0.001 and k<1000: # loop through all blocks in grid
+    X = R + gamma*P.dot(v)
+    v_kplus1 =  v
+    arr.append(v_kplus1)
+    v = np.amax(X,axis=0)
+    
+    diff = np.linalg.norm(v-v_kplus1)
 #    print(v.reshape(4,4))
-#    k += 1
+    k += 1
     
 ### Solvable states ###
 #[['0', '1', '2', '3'],
