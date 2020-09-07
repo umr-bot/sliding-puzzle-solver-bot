c64a0038d44e2d9ea45bceb5a9aff5ef9bc0f7a1
Umr Barends <ubarends@gmail.com>
2020-09-07 22:34:12 +0200

    Added q-learning version of RL

--- my_solver/RL/q-learning/agent.py
+++ my_solver/RL/q-learning/agent.py
@@ -18,32 +18,6 @@ class agent():
             action = np.argmax(self.Q[:,state])
         return action
 
-    def SARSA(self, gamma=0.9, alpha=0.3, epsilon=0.1):
-        episode = 0
-        cnt = 0
-        timestep_reward = []
-        while episode < 10000:
-            total_reward = 0
-            S = self.env.reset()
-            if episode % 5000 == 0 : print("Sarsa",episode)
-            A = self.choose_action(state=self.env.states.index(S), epsilon=epsilon)
-            for step in range(100):
-                S_, R, terminate = self.env.step(A)
-                total_reward += R
-                si = self.env.states.index(S)
-                si_ = self.env.states.index(S_)
-                A_ = self.choose_action(state=si_, epsilon=epsilon)
-                if terminate:
-                    self.Q[A,si] += alpha*(R - self.Q[A,si])
-                else:
-                    self.Q[A,si] += alpha*(R + (gamma*self.Q[A_,si_]) - self.Q[A,si])
-                S, A = S_, A_
-                if terminate:
-                    timestep_reward.append(total_reward)
-                    break
-            episode += 1
-        return timestep_reward
-    
     def q_learn(self, gamma=0.9, alpha=0.3, epsilon=0.1):
         episode = 0
         cnt = 0
@@ -51,7 +25,7 @@ class agent():
         while episode < 10000:
             total_reward = 0
             S = self.env.reset()
-            if episode % 5000 == 0 : print("Q-learn",episode)
+            if episode % 5000 == 0 : print(episode)
             A = self.choose_action(state=self.env.states.index(S), epsilon=epsilon)
             for step in range(100):
                 S_, R, terminate = self.env.step(A)
@@ -69,39 +43,25 @@ class agent():
                     break
             episode += 1
         return timestep_reward
-
 ############## TESTING ###############
-eps = [0.1]
-##########
+eps = [0,0.01,0.1]
 r = dict() # random placeholder values in the lise
 for i in range(len(eps)):
     agent_ = agent(np.zeros((4,24)))
-    r[i] = agent_.SARSA(epsilon=eps[i],alpha=0.4,gamma=0.999)
-
+    r[i] = agent_.q_learn(epsilon=eps[i])
+    #print(agent.Q.T)
 plt_data = dict()
+
 for i in range(len(r)):
     r_plt = []
     for j in range(1,len(r[i])+1): 
         r_plt.append(sum(r[i][0:j])/j)
     plt_data[i] = r_plt
-    lab = 'sarsa_eps=' + str(eps[i])
-    plt.plot(r[i],label=lab)
-plt.legend(loc="lower right")
-###########
-r = dict()
-for i in range(len(eps)):
-    agent_ = agent(np.zeros((4,24)))
-    r[i] = agent_.q_learn(epsilon=eps[i],alpha=0.4,gamma=0.999)
+    lab = 'eps=' + str(eps[i])
+    plt.plot(r_plt[i],label=lab)
 
-plt_data = dict()
-for i in range(len(r)):
-    r_plt = []
-    for j in range(1,len(r[i])+1): 
-        r_plt.append(sum(r[i][0:j])/j)
-    plt_data[i] = r_plt
-    lab = 'q_learn_eps=' + str(eps[i])
-    plt.plot(r[i],label=lab)
-
+#ax = plt.gca() # not really neccesary to save the axes since its only usde once
+#ax.legend(('e0'+str(eps[0]),'e1='+str(eps[1]),'e2='+str(2)))
 plt.legend(loc="lower right")
 plt.show()
 
--- my_solver/RL/sarsa/agent.py
+++ my_solver/RL/sarsa/agent.py
@@ -44,64 +44,38 @@ class agent():
             episode += 1
         return timestep_reward
     
-    def q_learn(self, gamma=0.9, alpha=0.3, epsilon=0.1):
-        episode = 0
-        cnt = 0
-        timestep_reward = []
-        while episode < 10000:
-            total_reward = 0
-            S = self.env.reset()
-            if episode % 5000 == 0 : print("Q-learn",episode)
-            A = self.choose_action(state=self.env.states.index(S), epsilon=epsilon)
-            for step in range(100):
-                S_, R, terminate = self.env.step(A)
-                total_reward += R
-                si = self.env.states.index(S)
-                si_ = self.env.states.index(S_)
-                A_ = np.argmax(self.Q[:, si_])
-                if terminate:
-                    self.Q[A,si] += alpha*(R - self.Q[A,si])
-                else:
-                    self.Q[A,si] += alpha*(R + (gamma*self.Q[A_,si_]) - self.Q[A,si])
-                S, A = S_, A_
-                if terminate:
-                    timestep_reward.append(total_reward)
-                    break
-            episode += 1
-        return timestep_reward
-
 ############## TESTING ###############
-eps = [0.1]
+eps = [0.1,0.4]
 ##########
 r = dict() # random placeholder values in the lise
 for i in range(len(eps)):
     agent_ = agent(np.zeros((4,24)))
     r[i] = agent_.SARSA(epsilon=eps[i],alpha=0.4,gamma=0.999)
-
+################## Plotting average reward values
 plt_data = dict()
 for i in range(len(r)):
     r_plt = []
     for j in range(1,len(r[i])+1): 
         r_plt.append(sum(r[i][0:j])/j)
     plt_data[i] = r_plt
-    lab = 'sarsa_eps=' + str(eps[i])
-    plt.plot(r[i],label=lab)
-plt.legend(loc="lower right")
-###########
-r = dict()
-for i in range(len(eps)):
-    agent_ = agent(np.zeros((4,24)))
-    r[i] = agent_.q_learn(epsilon=eps[i],alpha=0.4,gamma=0.999)
-
-plt_data = dict()
-for i in range(len(r)):
-    r_plt = []
-    for j in range(1,len(r[i])+1): 
-        r_plt.append(sum(r[i][0:j])/j)
-    plt_data[i] = r_plt
-    lab = 'q_learn_eps=' + str(eps[i])
-    plt.plot(r[i],label=lab)
+    lab = 'eps=' + str(eps[i])
+    plt.plot(r_plt,label=lab)
 
 plt.legend(loc="lower right")
 plt.show()
+###############################################
 
+################## Plotting optimal % values
+plt_data = dict()
+for i in range(len(r)):
+    r_plt = []
+    for j in range(1,len(r[i])+1): 
+        r_plt.append(sum(r[i][0:j])/j)
+    plt_data[i] = r_plt
+    lab = 'eps=' + str(eps[i])
+    r_plt = np.array(r_plt)
+    plt.plot((r_plt/r[i][-1])*100,label=lab)
+
+plt.legend(loc="lower right")
+plt.show()
+###############################################
